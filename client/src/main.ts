6d153108efce61a94f4b0631c1805a5c4c1ff673
tdtc-hrb <tdtc.hrb@gmail.com>
2024-06-07 19:42:52 -0700

    Restore main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,22 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import isCordova from './isCordova';
-
-const bootstrap = () => {
-	platformBrowserDynamic().bootstrapModule(AppModule, {
-	  ngZoneEventCoalescing: true
-	})
-	  .catch(err => console.error(err));
-};
-if (isCordova()) {
-  document.addEventListener(
-    "deviceready",
-    () => {
-      bootstrap();
-    },
-    false
-  );
-} else {
-  bootstrap();
-}
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true
+})
+  .catch(err => console.error(err));
